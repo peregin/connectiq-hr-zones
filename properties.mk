8a02d4fd0b64c63c1021a119c466f043305b3773
levi <velocorner.com@gmail.com>
2023-06-22 22:57:02 +0200

    update

--- properties.mk
+++ properties.mk
@@ -1,4 +1,4 @@
 DEVICE = edge830
-SDK_HOME = "/Users/levi/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-4.1.4-2022-06-07-f86da2dee"
+SDK_HOME = "/Users/levi/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk-mac-4.2.4-2023-04-05-5830cc591"
 DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
 PRIVATE_KEY = /Users/levi/Downloads/velo/iq-connect/developer_key.der
